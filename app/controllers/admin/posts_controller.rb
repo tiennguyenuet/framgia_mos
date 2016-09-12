97691bb66e95e141e6bcc46cd8e2bc33dd4c3b3a
Do Gia Dat <dogiadat@gmail.com>
2016-09-12 11:29:01 +0700

    admin static page

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,5 @@
+class Admin::PostsController < ApplicationController
+  layout "admin"
+  def index
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module BaseApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "admin", "*.yml")]
   end
 end
