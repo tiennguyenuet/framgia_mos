f5059aab53a1284589e4b7a1fd6234d6ff8da6d6
Do Gia Dat <dogiadat@gmail.com>
2016-10-13 10:48:51 +0700

    fix-bug-admin

--- config/application.rb
+++ config/application.rb
@@ -14,13 +14,14 @@ module BaseApp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.time_zone = "Hanoi"
+    config.active_record.default_timezone = :local
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
