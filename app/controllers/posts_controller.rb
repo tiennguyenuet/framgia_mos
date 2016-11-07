d4b814474f83c6345880c91db0bf962cef57429d
Do Gia Dat <dogiadat@gmail.com>
2016-11-07 14:38:23 +0700

    giao dien va nguoi dung gui bai dang

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,30 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
+  before_action :load_all_categories, only: :new
+
+  def new
+  end
+
+  def create
+    if @post.save
+      flash[:success] = t ".success"
+      redirect_to root_url
+    else
+      load_all_categories
+      render :new
+    end
+  end
 
   def show
+  end
+
+  private
+  def load_all_categories
+    @nested_categories = Category.all_categories @category_roots
+  end
+
+  def post_params
+    params.require(:post).permit :id, :title, :description, :category_id,
+      :content, :image, :audio, :user_id, :post_type, :status
   end
 end
