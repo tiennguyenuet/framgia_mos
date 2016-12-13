f441a922c72922be8e12bbc3f11dda11eb898f8e
Do Gia Dat <dogiadat@gmail.com>
2016-12-13 15:27:27 +0700

    phan quyen

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -8,4 +8,11 @@ class Admin::BaseController < ApplicationController
   def check_admin_logged_in
     redirect_to new_admin_user_session_path unless admin_user_signed_in?
   end
+
+  def current_ability
+    controller_name_segments = params[:controller].split("/")
+    controller_name_segments.pop
+    controller_namespace = controller_name_segments.join("/").camelize
+    Ability.new(current_admin_user, controller_namespace)
+  end
 end
